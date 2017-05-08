675563448564fa3d4a4d2deba8c7a7490ef6825b
DCSaunders <ds636@cam.ac.uk>
2017-05-08 17:53:46 +0100

    wmap_grammar produces id for each rule, not each token. Move label_paren to tree-edit repo

--- wordmap_grammar.py
+++ wordmap_grammar.py
@@ -8,25 +8,10 @@ import sys
 def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('-v', '--vocab_size', type=int, default=0, help='Vocab size for wmap, 0 if whole vocab')
-    parser.add_argument('-l', '--lowercase', default=False, action='store_true', help='True if lowercasing')
     parser.add_argument('-s', '--split_grammar', default='==>', help='Grammar rule delineator')
     parser.add_argument('-w', '--wmap_out', default='/tmp/wmap', help='Location to save new wmap')
-    parser.add_argument('-o', '--grammar_out', default='/tmp/wmap', help='Location to save mapped grammar')
+    parser.add_argument('-o', '--grammar_out', default='/tmp/grammar', help='Location to save mapped grammar')
     return parser.parse_args()
-
-def prods_to_eq(prods):
-    seq = [prods[0].lhs()]
-    for prod in prods:
-        if str(prod.lhs()) == 'Nothing':
-            break
-        for ix, s in enumerate(seq):
-            if s == prod.lhs():
-                seq = seq[:ix] + list(prod.rhs()) + seq[ix+1:]
-                break
-    try:
-        return ''.join(seq)
-    except:
-        return ''
 
 eps = '<epsilon>'
 args = get_args()
@@ -40,20 +25,16 @@ for idx, nt in enumerate(rules, start=1):
     ids[nt] = str(idx)
 rules[eps].append([eps])
 
-with open(args.grammar_out, 'w') as f:
+wmap_idx = 0
+with open(args.grammar_out, 'w') as f, open(args.wmap_out, 'w') as f_w:
     for nt in rules:
         for rule in rules[nt]:
             rule_out = []
             for r in rule:
                 if r not in ids:
-                    if args.lowercase:
-                        r = r.lower()
                     ids[r] = str(len(ids))
                 rule_out.append(ids[r])
             f.write('{} : {}\n'.format(ids[nt], ' '.join(rule_out)))
-
-reverse = {v: k for k, v in ids.items()}
-with open(args.wmap_out, 'w') as f:
-    for i in range(len(ids)):
-        idx = str(i)
-        f.write('{} {}\n'.format(reverse[idx], idx))
+            f_w.write('"{} {} {}" {}\n'.format(
+                nt, args.split_grammar, ' '.join(rule), wmap_idx))
+            wmap_idx += 1
