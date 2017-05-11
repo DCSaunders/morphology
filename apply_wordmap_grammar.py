edc90ea89a1dd3dd80cf5a8952dddd318523b547
DCSaunders <ds636@cam.ac.uk>
2017-05-11 15:42:28 +0100

    working forward write/apply wmap

--- apply_wordmap_grammar.py
+++ apply_wordmap_grammar.py
@@ -1,15 +1,20 @@
 import argparse
 import numpy as np
 import collections
-import re
 import sys
 
 
 def get_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-d', '--delimit_grammar', default='//', help='Grammar rule delineator')
-    parser.add_argument('-w', '--wmap_in', help='Location to save new wmap')
-    parser.add_argument('-o', '--out_idx', default='/tmp/rules.idx', help='Location to save mapped grammar')
+    parser.add_argument('-d', '--delimit_grammar', default='//', 
+                        help='Grammar rule delimiter')
+    parser.add_argument('-w', '--wmap_in', help='Location to load wmap')
+    parser.add_argument('-o', '--out_idx', default='/tmp/idx', help='Location to save mapped out')
+    parser.add_argument('-i2g', '--to_grammar', default=False, 
+                        action='store_true', help='True if idx->rules')
+    parser.add_argument('-i2w', '--to_words', default=False, 
+                        action='store_true', help='True if idx->words')
+
     return parser.parse_args()
 
 args = get_args()
@@ -20,6 +25,16 @@ with open(args.wmap_in, 'r') as f:
         split = line.split()
         rule_map[' '.join(split[:-1])] = split[-1]
 
+if args.to_words or args.to_grammar:
+    # TODO
+    # If mapping to words, need to map to grammar rules first, reverse wmap,
+    # and have set of non-terminals to cut out after reversing
+    # space-join if idx or word out. delimiter-join if grammar out.
+    joiner = ' {} '.format(args.delimit_grammar)
+else:
+    joiner = ' '
+
+
 with open(args.out_idx, 'w') as f:
     for line in sys.stdin:
         productions = line.split(args.delimit_grammar)
@@ -27,4 +42,4 @@ with open(args.out_idx, 'w') as f:
         for prod in productions:
             if prod.strip():
                 out.append(rule_map[prod.strip()])
-        f.write('{}\n'.format(' '.join(out)))
+        f.write('{}\n'.format(joiner.join(out)))
--- write_wordmap_grammar.py
+++ write_wordmap_grammar.py
@@ -13,28 +13,41 @@ def get_args():
     parser.add_argument('-o', '--grammar_out', default='/tmp/grammar', help='Location to save mapped grammar')
     return parser.parse_args()
 
+def output_rule(nt, rules, ids, wmap_idx, f, f_w):
+    for rule_rhs in rules[nt]:
+        rule_out = []
+        for r in rule_rhs:
+            if len(rule_rhs) == 1 and r == nt and r not in (eps, unk):
+                # some terminals are identical to non-terminals e.g. '.'
+                r = '{}-T'.format(r) 
+            if r not in ids:
+                ids[r] = str(len(ids))
+            rule_out.append(ids[r])
+        f.write('{} : {}\n'.format(ids[nt], ' '.join(rule_out)))
+        f_w.write('{} {} {} {}\n'.format(
+        nt, args.split_grammar, ' '.join(rule_rhs), wmap_idx))
+        wmap_idx += 1
+    return wmap_idx
+
 eps = '<epsilon>'
+unk = 'UNK'
 args = get_args()
-rules = collections.defaultdict(list)
-ids = {eps: '0'}
+rules = collections.defaultdict(list) # maps nt to list of split RHSs
+ids = {eps: '0', unk: '1'}
 for line in sys.stdin:
     lhs, rhs = line.strip('\n').split(args.split_grammar)
     rules[lhs.strip()].append(rhs.split())
 
-for idx, nt in enumerate(rules, start=1):
+for idx, nt in enumerate(rules, start=2):
     ids[nt] = str(idx)
 rules[eps].append([eps])
+rules[unk].append([unk])
 
-wmap_idx = 0
 with open(args.grammar_out, 'w') as f, open(args.wmap_out, 'w') as f_w:
+    output_rule(eps, rules, ids, 0, f, f_w)
+    output_rule(unk, rules, ids, 1, f, f_w)
+    wmap_idx = 2
+    rules.pop(eps)
+    rules.pop(unk)
     for nt in rules:
-        for rule in rules[nt]:
-            rule_out = []
-            for r in rule:
-                if r not in ids:
-                    ids[r] = str(len(ids))
-                rule_out.append(ids[r])
-            f.write('{} : {}\n'.format(ids[nt], ' '.join(rule_out)))
-            f_w.write('{} {} {} {}\n'.format(
-                nt, args.split_grammar, ' '.join(rule), wmap_idx))
-            wmap_idx += 1
+        wmap_idx = output_rule(nt, rules, ids, wmap_idx, f, f_w)
