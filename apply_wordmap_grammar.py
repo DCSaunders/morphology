6a30f8bdd57c01373fcfbe06ce5ebe8b79cb56c6
DCSaunders <ds636@cam.ac.uk>
2017-05-24 23:12:34 +0100

    improved terminal checking

--- apply_wordmap_grammar.py
+++ apply_wordmap_grammar.py
@@ -8,6 +8,7 @@ def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('-d', '--delimit_grammar', default='//', help='Grammar rule delimiter')
     parser.add_argument('-u', '--UNK', default='UNK ==> UNK', help='unknown symbol')
+    parser.add_argument('-ut', '--TERMINAL_UNK', default='<unk>')
     parser.add_argument('-w', '--wmap_in', help='Location to load wmap')
     parser.add_argument('-o', '--out_idx', default='/tmp/idx', help='Location to save mapped out')
     parser.add_argument('-i2g', '--to_grammar', default=False, 
@@ -17,32 +18,63 @@ def get_args():
 
     return parser.parse_args()
 
+ambig_terminals = set(['-RRB', '-LRB', 'N'])
+
+def is_terminal(tok, nt, nt_list):
+    if tok not in nt_list:
+        return True
+    else:
+        if tok.lower() == tok or tok in ambig_terminals:
+            return True
+    return False
+
+
+def mapped_prod(prod, rule_map, nt_set):
+    if args.to_words:
+        lhs, rhs = rule_map[prod].split('==>')
+        rhs_split = rhs.strip().split()
+        if len(rhs_split) == 1 and is_terminal(rhs_split[0], lhs, nt_set):
+            return rhs_split[0]
+        else:
+            return ''
+    else:
+        return rule_map[prod]
+
+def unk_or_unk_t(prod, rule_map, out, nt_set):
+    lhs, rhs = prod.split(' ==> ')
+    rhs_split = rhs.split()
+    if len(rhs_split) == 1 and is_terminal(rhs_split[0], lhs, nt_set):
+        out.append(rule_map['{} ==> {}'.format(lhs, args.TERMINAL_UNK)])
+    else:
+        out.append(rule_map[args.UNK])
+
+
 args = get_args()
 rule_map = {}
-
+nt_set = set()
 with open(args.wmap_in, 'r') as f:
     for line in f:
         split = line.split()
         rule_map[' '.join(split[:-1])] = split[-1]
+        nt_set.add(split[0])
 
 if args.to_words or args.to_grammar:
-    # TODO
-    # If mapping to words, need to map to grammar rules first, reverse wmap,
-    # and cut out non-terminals
-    # space-join if idx or word out. delimiter-join if grammar out.
-    joiner = ' {} '.format(args.delimit_grammar)
+    rule_map = {v : k for k, v in rule_map.items()}
+    joiner = ' {} '.format(args.delimit_grammar) if args.to_grammar else ' '
+    delimiter = ' '
 else:
     joiner = ' '
+    delimiter = args.delimit_grammar
 
 
 with open(args.out_idx, 'w') as f:
     for line in sys.stdin:
-        productions = line.split(args.delimit_grammar)
+        productions = line.split(delimiter)
         out = []
         for prod in productions:
             prod = prod.strip()
             if prod in rule_map:
-                out.append(rule_map[prod])
+                out.append(mapped_prod(prod, rule_map, nt_set))
             elif prod:
-                out.append(rule_map[args.UNK])
+                unk_or_unk_t(prod, rule_map, out, nt_set)
         f.write('{}\n'.format(joiner.join(out)))
--- write_wordmap_grammar.py
+++ write_wordmap_grammar.py
@@ -14,13 +14,15 @@ def get_args():
     parser.add_argument('-t', '--train_in', default=None, help='Location of training data')
     return parser.parse_args()
 
+ambig_terminals = set(['-RRB', '-LRB', 'N'])
+
 def output_rule(nt, rules, ids, wmap_idx, f, f_w, most_common=None):
     for rule_rhs in rules[nt]:
         whole_rule = '{} {} {}'.format(nt, args.split_grammar, ' '.join(rule_rhs))
         if not most_common or whole_rule in most_common:
             rule_out = []
             for r in rule_rhs:
-                if len(rule_rhs) == 1 and r == nt and r not in (eps, unk):
+                if len(rule_rhs) == 1 and is_terminal(r, nt, rules.keys()) and r in rules.keys():
                     # some terminals are identical to non-terminals e.g. '.'
                     r = '{}-T'.format(r) 
                 if r not in ids:
@@ -31,42 +33,70 @@ def output_rule(nt, rules, ids, wmap_idx, f, f_w, most_common=None):
             wmap_idx += 1
     return wmap_idx
 
+def is_terminal(tok, nt, nt_list):
+    if tok not in nt_list:
+        return True
+    else:
+        if tok.lower() == tok or tok in ambig_terminals:
+            return True
+    return False
+    
+
 delimiter = '//'
 eps = '<epsilon>'
 unk = 'UNK'
+unk_t = '<unk>'
 start = 'START'
 root = 'ROOT'
+eos = 'EOS'
 args = get_args()
 rule_counts = collections.Counter()
 rules = collections.defaultdict(list) # maps nt to list of split RHSs
-ids = {eps: '0', start: '1', unk: '2'}
+ids = {eps: '0', start: '1', eos: '2', unk: '3'}
 
 if args.train_in:
     with open(args.train_in) as f:
         for line in f:
             line_rules = line.split(delimiter)
-            rule_counts.update([rule.strip() for rule in line_rules 
-                                if rule.strip()])
+            for rule in line_rules:
+                rule = rule.strip()
+                if rule not in rule_counts:
+                    lhs, rhs = rule.split(args.split_grammar)
+                    lhs = lhs.strip()
+                    rhs = rhs.strip().split()
+                    rules[lhs].append(rhs)
+                rule_counts.update([rule])
 
 most_common = set()
-if args.vocab_size:
-    for rule, _ in rule_counts.most_common(args.vocab_size):
-        most_common.add(rule)
-        lhs, rhs = rule.split(args.split_grammar)
-        rules[lhs.strip()].append(rhs.split())
+for lhs in rules:
+    terminal_est = len([rhs for rhs in rules[lhs] 
+                        if len(rhs) == 1 and is_terminal(rhs[0], lhs, rules.keys())])
+    if terminal_est > 1:
+        unk_t_rule = '{} {} {}'.format(lhs, args.split_grammar, unk_t)
+        most_common.add(unk_t_rule)
+        rules[lhs].append([unk_t])
 
-for idx, nt in enumerate(rules, start=3):
+if args.vocab_size:
+    for rule, _ in rule_counts.most_common(args.vocab_size - len(most_common)):
+        most_common.add(rule)
+
+
+for idx, nt in enumerate(rules, start=len(ids)):
     ids[nt] = str(idx)
-rules[eps].append([eps])
-rules[unk].append([unk])
-rules[start].append([root])
+rules[eps].append([eps]) # epsilon rule
+rules[start].append([root, eos]) # start rule
+rules[eos].append([eps]) # eos rule
+rules[unk].append([unk]) # unk rule
+
 with open(args.grammar_out, 'w') as f, open(args.wmap_out, 'w') as f_w:
     output_rule(eps, rules, ids, 0, f, f_w)
     output_rule(start, rules, ids, 1, f, f_w)
-    output_rule(unk, rules, ids, 2, f, f_w)
-    wmap_idx = 3
+    output_rule(eos, rules, ids, 2, f, f_w)
+    output_rule(unk, rules, ids, 3, f, f_w)
+    wmap_idx = 4
     rules.pop(eps)
-    rules.pop(unk)
     rules.pop(start)
+    rules.pop(eos)
+    rules.pop(unk)
     for nt in rules:
         wmap_idx = output_rule(nt, rules, ids, wmap_idx, f, f_w, most_common)
